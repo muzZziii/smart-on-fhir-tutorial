d9353c5e0e5d34e9249aee7b22e98485f95a7003
Muzamil Ashfaq <muzamilashfaq7@gmail.com>
2020-01-14 17:54:39 +0500

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -30,7 +30,7 @@
 
         $.when(pt, obv).fail(onError);
 
-        $.when(pt).done(function(patient) {
+        $.when(pt, obv).done(function(patient, obv) {
           var byCodes = smart.byCodes(obv, 'code');
           var gender = patient.gender;
 
